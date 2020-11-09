3e7040b01a17b8e56b741445618705997f46fee1
Minhaz <zubair.islam143@gmail.com>
2020-11-10 01:25:27 +0600

    Profile_view_profile.php file is added

---
+++ LAB_TASK_4.1_SESSION/Profile_View_Profile.php
@@ -0,0 +1,55 @@
+<?php
+session_start();
+
+if(!isset($_SESSION['active']))
+{
+    header('location: public.html');
+}
+?>
+<?php
+include_once('userloginheader.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+</head>
+<body>
+    <table align="center" width='50%' border="1">
+        <tr>
+            <td>
+                <h3>Account</h3>
+                <hr>
+                <ul>
+                    <li><a href="profile_dash.php">Dashboard</a></li>
+                    <li><a href="profile_view_profile.php">View Profile</a></li>
+                    <li><a href="profile_edit_profile.php">Edit Profile</a></li>
+                    <li><a href="profile_change_pic.php">Change Profile Picture</a></li>
+                    <li><a href="profile_change_pas.php">Change Password</a></li>
+                    <li><a href="logout.php">Log Out</a></li>
+                </ul>                
+            </td>
+            <td>
+                Name :  <?php echo $_SESSION['name']; ?> <br>
+                Email : <?php echo $_SESSION['email']; ?>  <br>
+                Gender : <?php echo $_SESSION['gen']; ?>   <br>
+                Date Of Birth : <?php echo $_SESSION['dob']; ?>  <br>
+
+                <a href="profile_edit_profile.php">Edit Profile</a>
+            </td>
+            <td>
+                <input type="file" name="image" id="" value="">
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
+
+<?php
+
+    include_once('footer.html');
+?>
